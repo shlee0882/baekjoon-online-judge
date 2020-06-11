8da118b7f8701f1cc70081007b5f25332849d528
SangHyun Lee <shlee0882@gmail.com>
2020-06-11 17:56:52 +0900

    Update 완주하지못한선수.py

--- programmers/level1/완주하지못한선수.py
+++ programmers/level1/완주하지못한선수.py
@@ -1,17 +1,12 @@
 # 완주하지 못한 선수
 # 효율성 실패함...
-def solution(participant, completion):
-
-    while True:
-        if len(completion) > 0:
-            completeRunner = completion.pop()
-            for i,j in enumerate(participant):
-                if completeRunner == j:
-                    participant.remove(j)
-                    break
-        else:
-            break
-        
-    return participant[0]
+def solution(p, c):
+    p.sort()
+    c.sort()
+    for i,j in zip(p,c):
+        if i != j:
+            return i
+    
+    return p[-1]
 
 solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"])
