a1092d4ffcdf8aa99aff43b2486902529423412a
SangHyun Lee <shlee0882@gmail.com>
2020-06-22 22:40:11 +0900

    Update 같은숫자는 싫어.java

--- java/programmers/level1/같은숫자는 싫어.java
+++ java/programmers/level1/같은숫자는 싫어.java
@@ -1,6 +1,15 @@
-    public int[] solution(int[] arr) {
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+       int[] arr = {55,55,46,3,0,1,1};
+        System.out.println(solution(arr));
+    }
+
+    public static int[] solution(int[] arr) {
         int current, next =0;
         List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
 
         for(int i=0; i<arr.length; i++){
             list1.add(arr[i]);
@@ -11,9 +20,12 @@
             current = list1.get(i);
             if (i + 1 < list1.size()) {
                 next = list1.get(i + 1);
-                if (current == next) {
-                    list1.remove(i+1);
-                    i=0;
+                if(i == 0){
+                    list2.add(current);
+                }
+                if (current != next) {
+                    list2.add(next);
+                    i++;
                 }else{
                     i++;
                 }
@@ -22,10 +34,12 @@
             }
         }
 
-        int[] answer = new int[list1.size()];
-
-        for(int j=0; j<list1.size(); j++){
-            answer[j] = list1.get(j);        
+        int[] answer = new int[list2.size()];
+        for(int k=0; k<list2.size();k++){
+            System.out.println(list2.get(k));
+            answer[k] = list2.get(k);
         }
+
         return answer;
     }
+}
