509bd77971b8982499f998d22bc09e5ec48bb021
이상현 <shlee0882@gmail.com>
2020-07-16 21:32:41 +0900

    modify code

--- java/leetcode/easy/1436. Destination City.java
+++ java/leetcode/easy/1436. Destination City.java
@@ -1,53 +1,43 @@
 import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		
-		// ÀÛ¼ºÁß....
-		
+    public static void main(String[] args) {
+//      test case writing
 //		String[][] tmp = {{"London","New York"},{"New York","Lima"},{"Lima","Sao Paulo"}};
-//		String[][] tmp = {{"B","C"},{"D","B"},{"C","A"}};
+		String[][] tmp = {{"B","C"},{"D","B"},{"C","A"}};
 //		String[][] tmp = {{"A","Z"}};
 //		String[][] tmp = {{"pYyNGfBYbm","wxAscRuzOl"},{"kzwEQHfwce","pYyNGfBYbm"}};
 //		String[][] tmp = {{"qMTSlfgZlC","ePvzZaqLXj"},{"xKhZXfuBeC","TtnllZpKKg"},{"ePvzZaqLXj","sxrvXFcqgG"},{"sxrvXFcqgG","xKhZXfuBeC"},{"TtnllZpKKg","OAxMijOZgW"}};
-		
-		String[][] tmp = {{"jMgaf WaWA","iinynVdmBz"},{" QCrEFBcAw","wRPRHznLWS"},{"iinynVdmBz","OoLjlLFzjz"},{"OoLjlLFzjz"," QCrEFBcAw"},{"IhxjNbDeXk","jMgaf WaWA"},{"jmuAYy vgz","IhxjNbDeXk"}};
-		List paths = new ArrayList<>();	
-		
-		int index = 0;
-		for (String[] str: tmp) {
-			paths.add(Arrays.asList(str));
-			index++;
-		}
-		destCity(paths);
-		
-	}
+//        String[][] tmp = {{"jMgaf WaWA","iinynVdmBz"},{" QCrEFBcAw","wRPRHznLWS"},{"iinynVdmBz","OoLjlLFzjz"},{"OoLjlLFzjz"," QCrEFBcAw"},{"IhxjNbDeXk","jMgaf WaWA"},{"jmuAYy vgz","IhxjNbDeXk"}};
+        List paths = new ArrayList<>();
+
+        int index = 0;
+        for (String[] str: tmp) {
+            paths.add(Arrays.asList(str));
+            index++;
+        }
+        destCity(paths);
+
+    }
 
     public static String destCity(List<List<String>> paths) {
-//    	System.out.println(paths);
-    	List<String> tmpList = new ArrayList<>();	
-    	String first= "";
-    	String second= "";
-    	int idx = 0;
-    	for (List<String> list : paths) {
-    		System.out.println("second: "+second);
-    		System.out.println("list.get(0): "+list.get(0));
-    		
-    		if(!"".equals(first) && second.equals(list.get(0))) {
-    			tmpList.add(list.get(0));
-    			second= list.get(1);
-    		}
-			first = list.get(0);
-			if(idx == 0 ) {
-				second= list.get(1);
-			}
-    		idx++;
-		}
-    	
-    	System.out.println(tmpList);
-    	String answer = "";
-    	return answer;
+        Map<String, String> map = new HashMap<>();
+
+        for(List<String> innerList: paths){
+            map.put(innerList.get(0), innerList.get(1));
+        }
+        System.out.println(map);
+
+        for(Map.Entry entry: map.entrySet()){
+            String city = (String)entry.getValue();
+            System.out.println(city);
+
+            if(!map.containsKey(city)){
+                System.out.println("ìµœì¢…ê°’: "+city);
+                return city;
+            }
+
+        }
+        return "";
     }
-    
 }
