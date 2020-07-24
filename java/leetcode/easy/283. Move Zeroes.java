a16aa8896c06bd3dd2778754d01a0871a53343da
SangHyun Lee <shlee0882@gmail.com>
2020-07-24 22:33:16 +0900

    add daily code

---
+++ java/leetcode/easy/283. Move Zeroes.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+        moveZeroes(nums);
+    }
+
+    public static void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int insertPos = 0;
+        for (int num: nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
